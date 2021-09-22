312c950bddf19b4a152f66f04706ef124f584a8b
George Sydby <george.sydby@gmail.com>
2021-09-22 09:56:19 +0200

    Add project files.

---
+++ ExcercisesPDF/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ExcercisesPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var keepAlive = true;
+            while (keepAlive)
+            {
+                try
+                {
+                    Console.Write("Enter assignment number (or -1 to exit): ");
+                    var assignmentChoice = int.Parse(Console.ReadLine() ?? "");
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    switch (assignmentChoice)
+                    {
+                        case 1:
+                            RunExerciseOne();
+                            break;
+                        case 2:
+                            RunExerciseOne();
+                            break;
+                        case 3:
+                            // Call your next assignment method here!
+                            break;
+                        case -1:
+                            keepAlive = false;
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("That is not a valid assignment number!");
+                            break;
+                    }
+                    Console.ResetColor();
+                    Console.WriteLine("Hit any key to continue!");
+                    Console.ReadKey();
+                    Console.Clear;
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("That is not a valid Assignment number!");
+                    Console.ResetColor();
+                }
+            }
+        }
+    }
+}
