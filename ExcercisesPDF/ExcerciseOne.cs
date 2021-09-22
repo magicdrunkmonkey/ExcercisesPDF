7016d56ae9d17606c7eb3e53107c9bed8611dda7
George Sydby <george.sydby@gmail.com>
2021-09-22 10:34:43 +0200

    Excercise 1 - Done

--- ExcercisesPDF/ExcerciseOne.cs
+++ ExcercisesPDF/ExcerciseOne.cs
@@ -8,7 +8,9 @@ namespace ExcercisesPDF
     {
         public static void RunExcerciseOne()
         {
-            Console.WriteLine("You successfully ran excercise one!");
+            string firstName = "George";
+            string lastName = "Sydby";
+            Console.WriteLine("Hello {0} {1}! I'm glad to inform you that you are the test subject of my very first assignment!", firstName, lastName);
         }
     }
 }
